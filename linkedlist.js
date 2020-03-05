f4c6444d7370db5e48d4ce62c0be655273702eb1
Alex <robert.perez6@g.austincc.edu>
2020-03-05 14:32:06 -0600

    tried 8 and 5

--- linkedlist.js
+++ linkedlist.js
@@ -108,13 +108,10 @@ class SLL  {
       previousNode.next=new _Node(item,currNode);
     }else if(position>counter2||counter2<=0){
       console.log("position too big for current array size of "+ counter2+ " adding it to the last index");
-      insertLast(item);
+      this.insertLast(item);
     }
   }
 }
-/* function isEmpty(ll){
-
-}*/
 function size(ll){
   let ptr= ll.head;
   let counter=0;
@@ -152,7 +149,7 @@ function findPrevious (ll, item) {
     console.log('Item not found');
     return;
   }///123567  //4 prevnode// 3 currno//5
-  return previousNode.value;
+  return previousNode;
 }
 
 function findLast (ll) {
@@ -161,19 +158,82 @@ function findLast (ll) {
   while (tempNode.next !== null) {
     tempNode = tempNode.next;
   }
-  return tempNode.value;
+  return tempNode;
+}                                             //b-a b-a
+function reverselist(ll){      ///  123456   6-5 4-5    
+    let lastone = findLast(ll);
+    const newH=findLast(ll);
+    let temp = findPrevious(ll,lastone.value)
+    const Size = size(ll)
+    let i = 0;
+    while(i<Size && temp!==null){
+        let a=temp.value;
+        lastone.next=temp; //creates new last one
+        lastone=temp;
+        temp = findPrevious(ll,a)//change the temp to the one before last value
+        i++
+        if(i==Size-1){
+            temp=null;
+        }
+    }
+    ll.head=newH;
+}
+function thirdfromend (ll) {
+    let tempNode = ll.head;
+    let previousNode = ll.head;
+    let secondpreviousNode = ll.head;
+
+    while (tempNode.next !== null) {
+        secondpreviousNode=previousNode
+        previousNode = tempNode;
+        tempNode = tempNode.next;
+    }
+    return secondpreviousNode;
+  }   
+  
+function middleoflist (ll) {
+    const Size = size(ll)
+    let middle =Math.floor(Size/2)
+    let tempNode = ll.head;
+    let i =0;
+    if(Size%2==0){
+        while (i<middle) {
+            tempNode = tempNode.next;
+            i++;
+        }
+    }
+    else{
+        while (i<middle+1) {
+            tempNode = tempNode.next;
+            i++;
+        }
+    }
+    return tempNode;
+
+}
+function cyclelist(ll){//1-2-3-1-23-123 
+    let tempnode=ll.head;
+    let tempnode2=ll.head;
+    while ((tempnode!==null) ) {
+        if(tempnode2!==tempnode.next){
+            return true
+        }
+        while((tempnode2!==null)){   
+            tempnode2=tempnode2.next;
+        }
+        tempnode = tempnode.next;
+    }
+    return false;
 }
 
 
 function main(){
-
-
   let list= new SLL();
   console.log("new list");
   list.insertFirst("Apollo");//end of list
   list.insertFirst("Boomer");
   list.insertFirst("Helo");
-  list.insertFirst("Husker");
+  list.insertFirst("Husker"); 
   list.insertFirst("Starbuck");
   list.insertFirst("Tauhida");//beggining list
   console.log(list);
@@ -184,7 +244,7 @@ function main(){
   list.insertAfter("Helo","Artemis");
   const b = list.find("Helo");
   console.log(b);
-  list.insertAt("Loki",4);
+  list.insertAt("Loki",2);
   const c = list.find("Husker");
   console.log(c);
   list.remove("Tauhida");
@@ -192,9 +252,16 @@ function main(){
   display(list);
   console.log(size(list));
   console.log(empty(list));
-  console.log(findPrevious(list, "Helo"));
-  console.log(findLast(list))
-
+  //console.log(findPrevious(list, "Helo").value);
+  console.log(findLast(list).value)
+  console.log("this is prevlist")
+  //display(list);
+  //reverselist(list);
+    console.log("this is reverselist")
+  //display(list);
+    console.log(thirdfromend(list).value);
+    console.log(middleoflist(list).value);
+    console.log(cyclelist(list))
 
 }
 main();
